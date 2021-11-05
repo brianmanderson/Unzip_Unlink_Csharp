c3563d0813fc86564ce6f314bde6a37182cc5cf1
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-11-05 10:31:41 -0700

    starting point

---
+++ Unzip_And_Unlink/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FellowOakDicom.Imaging;
+
+namespace Unzip_And_Unlink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FellowOakDicom.DicomFile.Open(@"test.dcm");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
