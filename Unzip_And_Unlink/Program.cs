05aeca94e864aff16b56440c04b0377accbbdfaf
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-11-08 10:46:26 -0800

    updated to check series instance UIDs and write new frames of reference

--- Unzip_And_Unlink/Program.cs
+++ Unzip_And_Unlink/Program.cs
@@ -1,14 +1,133 @@
 ﻿using System;
-using FellowOakDicom.Imaging;
+using FellowOakDicom;
+using System.Threading;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+
 
 namespace Unzip_And_Unlink
 {
     class Program
     {
+        static string[] file_paths = { @"\\ucsdhc-varis2\radonc$\BMAnderson\test", @"C:\Users\b5anderson\source\repos\Unzip_Unlink_Files" };
+        ///
+
+        static void NewFrameOfReference(string base_directory)
+        {
+            string[] all_directories = Directory.GetDirectories(base_directory);
+            string[] all_files;
+            string status_file, uid;
+
+            foreach (string directory in all_directories)
+            {
+                List<string> all_series_uids = new List<string>();
+                List<DicomUID> frame_of_reference_uids = new List<DicomUID>();
+                DicomUID new_frame_UID;
+                status_file = Path.Join(directory, "NewFrameOfRef.txt");
+                if (File.Exists(status_file))
+                {
+                    continue;
+                }
+                all_files = Directory.GetFiles(directory);
+                Thread.Sleep(3000);
+                while (all_files.Length != Directory.GetFiles(directory).Length)
+                {
+                    Console.WriteLine("Waiting for files to be fully transferred...");
+                    all_files = Directory.GetFiles(directory);
+                    Thread.Sleep(3000);
+                }
+                Console.WriteLine("Updating frames of reference...");
+                foreach (string dicom_file in all_files)
+                {
+                    if (dicom_file.EndsWith(".dcm"))
+                    {
+                        try
+                        {
+                            var file = DicomFile.Open(dicom_file, FileReadOption.ReadAll);
+                            
+                            if (file.Dataset.Contains(DicomTag.FrameOfReferenceUID))
+                            {
+                                string series_uid = file.Dataset.GetString(DicomTag.SeriesInstanceUID);
+                                if (!all_series_uids.Contains(series_uid))
+                                {
+                                    all_series_uids.Add(series_uid);
+                                    frame_of_reference_uids.Add(DicomUIDGenerator.GenerateDerivedFromUUID());
+                                }
+                                new_frame_UID = frame_of_reference_uids[all_series_uids.IndexOf(series_uid)];
+                                file.Dataset.AddOrUpdate(DicomTag.FrameOfReferenceUID, new_frame_UID);
+                                file.Save(dicom_file);
+                            }
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+                }
+                FileStream fid = File.OpenWrite(status_file);
+                fid.Close();
+                Console.WriteLine("Finished!");
+            }
+        }
+        static void RenameFolder(string base_directory, string unzipped_file_directory)
+        {
+            string[] all_files = Directory.GetFiles(unzipped_file_directory);
+            foreach (string dicom_file in all_files)
+            {
+                if (dicom_file.EndsWith(".dcm"))
+                {
+                    var file = DicomFile.Open(dicom_file);
+                    string patient_name = file.Dataset.GetString(DicomTag.PatientName).Replace('^', '_');
+                    Directory.Move(unzipped_file_directory, Path.Join(base_directory, patient_name));
+                    Console.WriteLine("Finished!");
+                    break;
+                }
+            }
+        }
+        static void UnzipFiles(string zip_file_directory)
+        {
+            string[] all_files = Directory.GetFiles(zip_file_directory);
+            Console.WriteLine("Running...");
+            foreach (string zip_file in all_files)
+            {
+                long file_size = zip_file.Length;
+                Thread.Sleep(3000);
+                while (zip_file.Length != file_size)
+                {
+                    Console.WriteLine("Waiting for file to be fully transferred...");
+                    file_size = zip_file.Length;
+                    Thread.Sleep(3000);
+                }
+                if (zip_file.EndsWith(".zip"))
+                {
+                    string file_name = Path.GetFileName(zip_file);
+                    string output_dir = Path.Join(zip_file_directory, file_name.Substring(0, file_name.Length - 4));
+                    if (!Directory.Exists(output_dir))
+                    {
+                        Directory.CreateDirectory(output_dir);
+                        Console.WriteLine("Extracting...");
+                        ZipFile.ExtractToDirectory(zip_file, output_dir);
+                        Console.WriteLine("Renaming Folder...");
+                        RenameFolder(zip_file_directory, output_dir);
+                        File.Delete(zip_file);
+                    }
+
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            FellowOakDicom.DicomFile.Open(@"test.dcm");
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                // First lets unzip the life images
+                foreach (string file_path in file_paths)
+                {
+                    Thread.Sleep(3000);
+                    UnzipFiles(zip_file_directory: file_path);
+                    NewFrameOfReference(file_path);
+                }
+            }
         }
     }
 }
