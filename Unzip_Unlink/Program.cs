620851a8f73ff11f9fd1730d77e2533af6102efd
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2023-04-12 11:23:40 -0700

    updates

--- Unzip_Unlink/Program.cs
+++ Unzip_Unlink/Program.cs
@@ -41,7 +41,73 @@ namespace Unzip_Unlink
                 }
             }
         }
-        public void Runner(string base_directory)
+        public static void MoveFolder(string moving_directory, string current_folder)
+        {
+            string folder_name = Path.GetFileName(current_folder);
+            string status_file = Path.Combine(moving_directory, folder_name, "NewFrameOfRef.txt");
+            if (!Directory.Exists(moving_directory))
+            {
+                Directory.CreateDirectory(moving_directory);
+            }
+            Directory.Move(current_folder, Path.Combine(moving_directory, folder_name));
+            if (File.Exists(status_file))
+            {
+                File.Delete(status_file);
+            }
+        }
+        public static void UpdatedFrameOfReference(string base_directory, string directory)
+        {
+            string status_file, overall_status, parsing_status;
+            Watcher folder_watcher_class = new Watcher(directory);
+            status_file = Path.Combine(directory, "NewFrameOfRef.txt");
+            overall_status = Path.Combine(base_directory, $"UpdatingFrameOfRef_{Path.GetFileName(directory)}.txt");
+            parsing_status = Path.Combine(base_directory, $"Parsing_{Path.GetFileName(directory)}.txt");
+            int counter = 0;
+            Thread.Sleep(5000);
+            while (folder_watcher_class.Folder_Changed)
+            {
+                counter++;
+                folder_watcher_class.Folder_Changed = false;
+                Console.WriteLine("Waiting for files to be fully transferred...");
+                Thread.Sleep(5000);
+                if (counter > 3)
+                {
+                    return;
+                }
+            }
+            if (File.Exists(status_file))
+            {
+                return;
+            }
+            if (!File.Exists(parsing_status))
+            {
+                FileStream fid_parsing_status = File.OpenWrite(parsing_status);
+                fid_parsing_status.Close();
+            }
+            Console.WriteLine("Parsing DICOM files...");
+            FrameOfReferenceClass dicomParser = new FrameOfReferenceClass();
+            dicomParser.Characterize_Directory(directory);
+            if (File.Exists(parsing_status))
+            {
+                File.Delete(parsing_status);
+            }
+            Console.WriteLine("Updating frames of reference...");
+            if (!File.Exists(overall_status))
+            {
+                FileStream fid_overallstatus = File.OpenWrite(overall_status);
+                fid_overallstatus.Close();
+            }
+            dicomParser.ReWriteFrameOfReference();
+            FileStream fid_status = File.OpenWrite(status_file);
+            fid_status.Close();
+            MoveFolder(moving_directory: Path.Combine(base_directory, "NewFinished"), current_folder: directory);
+            Console.WriteLine("Finished!");
+            if (File.Exists(overall_status))
+            {
+                File.Delete(overall_status);
+            }
+        }
+        static void Runner(string base_directory)
         {
             string[] dicom_files;
             string status_file, overall_status, parsing_status, moving_status;
@@ -109,9 +175,10 @@ namespace Unzip_Unlink
                     string all_file_paths = File.ReadAllText(file_paths_file);
                     foreach (string file_path in all_file_paths.Split('\n'))
                     {
-                        if (!file_paths.Contains(file_path))
+                        string file = file_path.Split('\r')[0];
+                        if (!file_paths.Contains(file))
                         {
-                            file_paths.Add(file_path);
+                            file_paths.Add(file);
                         }
                     }
                 }
@@ -153,9 +220,7 @@ namespace Unzip_Unlink
                         Thread.Sleep(3000);
                         try
                         {
-                            Utils.NewFrameOfReferenceDirectory(file_path);
-                            FrameOfReferenceClass frameOfReferenceClass = new FrameOfReferenceClass();
-                            frameOfReferenceClass.Characterize_Directory(file_path);
+                            Runner(file_path);
 
                         }
                         catch
