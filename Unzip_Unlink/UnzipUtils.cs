615755d65e40d9e0d0ef84fe6219cf0d19b01423
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2023-06-02 13:10:44 -0700

    create a single function for unzipping an individual file

--- Unzip_Unlink/UnzipUtils.cs
+++ Unzip_Unlink/UnzipUtils.cs
@@ -39,51 +39,55 @@ namespace Unzip_Unlink
                 }
             }
         }
+        public static void UnzipFile(string zip_file, string zip_file_directory)
+        {
+            string overall_status;
+            FileInfo zip_file_info = new FileInfo(zip_file);
+            Thread.Sleep(3000);
+            bool move_on = false;
+            int tries = 0;
+            Thread.Sleep(3000);
+            while (Unzipper.IsFileLocked(zip_file_info))
+            {
+                Console.WriteLine("Waiting for file to be fully transferred...");
+                tries += 1;
+                Thread.Sleep(3000);
+                if (tries > 2)
+                {
+                    move_on = true;
+                    break;
+                }
+            }
+            if (move_on)
+            {
+                Console.WriteLine("Taking too long, will come back and try again...");
+                return;
+            }
+            overall_status = Path.Combine(zip_file_directory, $"ExtractingZip.txt");
+            if (!File.Exists(overall_status))
+            {
+                FileStream fid_overallstatus = File.OpenWrite(overall_status);
+                fid_overallstatus.Close();
+            }
+            Unzipper.UnzipFile(zip_file);
+            string file_name = Path.GetFileName(zip_file);
+            string output_dir = Path.Combine(Path.GetDirectoryName(zip_file), file_name.Substring(0, file_name.Length - 4));
+            if (Directory.Exists(output_dir))
+            {
+                Console.WriteLine("Renaming Folder...");
+                RenameFolder(output_dir);
+            }
+            if (File.Exists(overall_status))
+            {
+                File.Delete(overall_status);
+            }
+        }
         public static void UnzipFiles(string zip_file_directory)
         {
             string[] all_files = Directory.GetFiles(zip_file_directory, "*.zip", SearchOption.AllDirectories);
-            string overall_status;
             foreach (string zip_file in all_files)
             {
-                FileInfo zip_file_info = new FileInfo(zip_file);
-                Thread.Sleep(3000);
-                bool move_on = false;
-                int tries = 0;
-                Thread.Sleep(3000);
-                while (Unzipper.IsFileLocked(zip_file_info))
-                {
-                    Console.WriteLine("Waiting for file to be fully transferred...");
-                    tries += 1;
-                    Thread.Sleep(3000);
-                    if (tries > 2)
-                    {
-                        move_on = true;
-                        break;
-                    }
-                }
-                if (move_on)
-                {
-                    Console.WriteLine("Taking too long, will come back and try again...");
-                    continue;
-                }
-                overall_status = Path.Combine(zip_file_directory, $"ExtractingZip.txt");
-                if (!File.Exists(overall_status))
-                {
-                    FileStream fid_overallstatus = File.OpenWrite(overall_status);
-                    fid_overallstatus.Close();
-                }
-                Unzipper.UnzipFile(zip_file);
-                string file_name = Path.GetFileName(zip_file);
-                string output_dir = Path.Combine(Path.GetDirectoryName(zip_file), file_name.Substring(0, file_name.Length - 4));
-                if (Directory.Exists(output_dir))
-                {
-                    Console.WriteLine("Renaming Folder...");
-                    RenameFolder(output_dir);
-                }
-                if (File.Exists(overall_status))
-                {
-                    File.Delete(overall_status);
-                }
+                UnzipFile(zip_file, zip_file_directory);
                 Console.WriteLine("Running...");
                 Thread.Sleep(3000);
             }
